de2dce90c8f51d6152a79066aefefcdf8def6488
Robin Hamre <robinhamre@Mac.home>
2025-12-04 18:08:35 +0100

    Oppdatert Sanity-konfig og build

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,14 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'n2z8nnny',
-    dataset: 'production'
+    projectId: 'w7wun21q',
+    dataset: 'production',
   },
-  deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/cli#auto-updates
-     */
-    autoUpdates: true,
-  }
 })
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,11 +1,17 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
 export default defineConfig({
   name: 'default',
   title: 'Stallhamre',
 
-  projectId: 'w7wzn21q',
+  projectId: 'w7wun21q',
   dataset: 'production',
 
   plugins: [structureTool(), visionTool()],
+
   schema: {
     types: schemaTypes,
   },
