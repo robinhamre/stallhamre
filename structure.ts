36d66a7c4c3536c84d8a5f6cae9425b976b452b0
Robin Hamre <robinhamre@Mac.home>
2026-03-16 14:16:29 +0100

    Update Sanity structure

---
+++ structure.ts
@@ -0,0 +1,88 @@
+import {StructureBuilder} from 'sanity/structure'
+
+export const structure = (S: StructureBuilder) =>
+  S.list()
+    .title('Content')
+    .items([
+
+      // 🐎 SPORT
+      S.listItem()
+        .title('Sport')
+        .child(
+          S.list()
+            .title('Sport')
+            .items([
+              S.documentTypeListItem('horse').title('Hest'),
+              S.documentTypeListItem('driver').title('Kusk'),
+              S.documentTypeListItem('track').title('Travbane'),
+              S.documentTypeListItem('result').title('Resultat'),
+              S.documentTypeListItem('resultCategory').title('Resultat-kategorier'),
+            ])
+        ),
+
+      // 📰 INNHOLD
+      S.listItem()
+        .title('Innhold')
+        .child(
+          S.list()
+            .title('Innhold')
+            .items([
+              S.documentTypeListItem('post').title('Innlegg'),
+              S.documentTypeListItem('reportage').title('Reportasjer'),
+              S.documentTypeListItem('reportageCategory').title('Reportasjekategorier'),
+              S.documentTypeListItem('tips').title('Tips'),
+              S.documentTypeListItem('tipsCategory').title('Tips-kategorier'),
+              S.documentTypeListItem('category').title('Kategori'),
+            ])
+        ),
+
+      // 👥 STALL / ANDELER
+      S.listItem()
+        .title('Stall / Andeler')
+        .child(
+          S.list()
+            .title('Stall / Andeler')
+            .items([
+              S.documentTypeListItem('owner').title('Eier'),
+              S.documentTypeListItem('shareOwner').title('Andelseier'),
+              S.documentTypeListItem('shareOffer').title('Andelstilbud'),
+              S.documentTypeListItem('shareManager').title('Andelsbestyrer'),
+            ])
+        ),
+
+      // 👨‍💼 ADMINISTRASJON
+      S.listItem()
+        .title('Administrasjon')
+        .child(
+          S.list()
+            .title('Administrasjon')
+            .items([
+              S.documentTypeListItem('staff').title('Personal'),
+              S.documentTypeListItem('supplier').title('Leverandør'),
+              S.documentTypeListItem('invoice').title('Viderefakturering'),
+            ])
+        ),
+
+      // 🖼 MEDIA
+      S.listItem()
+        .title('Media')
+        .child(
+          S.list()
+            .title('Media')
+            .items([
+              S.documentTypeListItem('imageLibrary').title('Bildebank'),
+            ])
+        ),
+
+      // 📊 HISTORIKK
+      S.listItem()
+        .title('Historikk')
+        .child(
+          S.list()
+            .title('Historikk')
+            .items([
+              S.documentTypeListItem('yearStatsHistoric').title('År-historisk'),
+            ])
+        ),
+
+    ])
